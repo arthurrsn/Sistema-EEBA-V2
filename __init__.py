3b5fc5833ea8c7f793461c30ceaa8a89d07dacec
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-04-18 12:19:17 -0300

    Update __init__.py
    Asking the user if they want to register a new person or edit information.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
+def login(worker):
+    print()
 
+def cadastro():
+
+def editar():
+
+def welcome():
+  print('SISTEMA EEBA')
+  respostas = {
+    'Cadastro' : ['CADASTRAR', 'NOVO', 'USUÁRIO', ' USUARIO', '[1]', '1'];
+    'Editar' : ['EDITAR', 'INFORMAÇÕES', 'INFORMACÕES', 'INFORMAÇOES', 'INFORMACOES']  
+  }
+  welcome = str(input('''[1] Cadastrar novo Usuário 
+[2] Editar informações
+-->''')).upper().split(' ')
+  if map(welcome) in respostas['Cadastro']:
+    cadastro()
+  elif welcome in respostas['Editar']:
+    editar()
+
+def main():
+        
